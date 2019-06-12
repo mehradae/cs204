4af3e6836e603342cab9dbd87d5722a5e1b44a1e
williamv1217 <williamv1217@gmail.com>
2019-06-11 22:42:13 -0700

    key change template

---
+++ TCP_no_delay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace CS204 {
+
+    public class TCP_no_delay {
+
+        public void Add() {
+            String path = "SOFTWARE\\Microsoft\\MSMQ";
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(path, true)) {
+                if (key != null) {
+                    key.SetValue("TCPNoDelay", "1", RegistryValueKind.DWord);
+                    key.Close();
+                }
+            }
+        }
+
+        public void Remove() {
+            String path = "SOFTWARE\\Microsoft\\MSMQ";
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(path, true)) {
+                if (key != null) {
+                    if (key.GetValue("TCPNoDelay") != null) {
+                        key.DeleteValue("TCPNoDelay");
+                        key.Close();
+                    }
+                }
+            }
+        }
+    }
+}
